0ef498e7502f23a281ddf023719c07f4d42cf398
Meik Felser <mkfelser@gmail.com>
2025-02-06 23:43:03 +0100

    make helper  getIntValue an inline function

--- software/src/huawei_tools.cpp
+++ software/src/huawei_tools.cpp
@@ -10,14 +10,3 @@ void delay(int seconds) {
 	QTimer::singleShot(seconds * 1000, &loop, SLOT(quit()));
     loop.exec();
 }
-
-qint32 getIntValue(const QVector<quint16> &values, int offset, int size)
-{
-	Q_ASSERT(size == 2);
-
-	qint32 v = 0;
-	for (int i=0; i<size; ++i) {
-		v = (v << 16) | values[offset + i];
-	}
-    return v;
-}
