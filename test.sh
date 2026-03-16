9174abe7b119c39889830df3a13d0298ae8da512
Meik Felser <mkfelser@gmail.com>
2026-03-16 21:52:44 +0000

    fix: add quotes to CC var

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 mkdir -p build/dbus-fronius-test
 cd build/dbus-fronius-test
-qmake CXX=$CXX ../../test/dbus-fronius-test.pro && make && ./dbus_fronius_test
+qmake CXX="$CXX" ../../test/dbus-fronius-test.pro && make && ./dbus_fronius_test
 if [[ $? -ne 0 ]] ; then
     exit 1
 fi
